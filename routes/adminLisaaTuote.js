dd9c13772794ff219871b1b5eb01ae682c8f5ddd
Anniina Kopsa <anniina@softarobotit.fi>
2024-11-05 13:55:48 +0200

    error message tuotteen lisäykseen

--- routes/adminLisaaTuote.js
+++ routes/adminLisaaTuote.js
@@ -2,31 +2,31 @@ let Tuote = require('../model/tuote');
 let Valokuva = require('../model/tuote');
 
 module.exports = (req, res) => {
-    console.log(req)
-    let BodyValokuvat = [];
-    if (req.query.valokuvat !== undefined) {
-        BodyValokuvat = req.query.valokuvat.array.forEach(BodyValokuva => new Valokuva({
-            url: BodyValokuva.url,
-            kuvateksti: BodyValokuva.kuvateksti
-        }));
-    }
+  console.log(req)
+  let BodyValokuvat = [];
+  if (req.query.valokuvat !== undefined) {
+    BodyValokuvat = req.query.valokuvat.array.forEach(BodyValokuva => new Valokuva({
+      url: BodyValokuva.url,
+      kuvateksti: BodyValokuva.kuvateksti
+    }));
+  }
 
-    let UusiTuote = new Tuote({
-        tuoteid: req.query.tuoteid,
-        nimi: req.query.nimi,
-        hinta: req.query.hinta,
-        lisatiedot: req.query.lisatiedot,
-        valokuvat: [BodyValokuvat]
+  let UusiTuote = new Tuote({
+    tuoteid: req.query.tuoteid,
+    nimi: req.query.nimi,
+    hinta: req.query.hinta,
+    lisatiedot: req.query.lisatiedot,
+    valokuvat: [BodyValokuvat]
+  });
+
+  UusiTuote
+    .save()
+    .then(data => {
+      console.log(data)
+      res.redirect('/admin/tuote?successMessage=Tuote%20on%20lisätty%20onnistuneesti'); // Uudelleenohjaus onnistuneen tallennuksen jälkeen viestin kanssa
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect(`/admin/tuote?errorMessage=${encodeURIComponent(err.message || "Tuotteen lisäys epäonnistui")}`); // Uudelleenohjaus epäonnistuneen tallennuksen jälkeen virheviestin kanssa
     });
-
-    UusiTuote
-        .save()
-        .then(data => {
-            res.redirect('/admin/tuote?successMessage=Tuote%20on%20lisätty%20onnistuneesti'); // Uudelleenohjaus onnistuneen tallennuksen jälkeen viestin kanssa
-        })
-        .catch(err => {
-            res.status(500).send({
-              message: err.message || "Some error occured during create operation"
-            })
-        });
 };
--- routes/uusiTuote.js
+++ routes/uusiTuote.js
@@ -1,3 +1,6 @@
 module.exports = (req, res) => {
-  res.render('../views/uusiTuote', { successMessage: req.query.successMessage })
+  res.render('../views/uusiTuote', { 
+    successMessage: req.query.successMessage,
+    errorMessage: req.query.errorMessage 
+  });
 };
