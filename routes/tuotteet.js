34842336d083ee749ed532f03a1c9a9b10b0b8d3
Anniina Kopsa <anniina@softarobotit.fi>
2024-11-05 10:18:43 +0200

    tuotteet error view

--- routes/tuotteet.js
+++ routes/tuotteet.js
@@ -4,11 +4,10 @@ module.exports = (req, res) => {
 
   Tuote.find()
   .then(tuote => {
-    console.log(tuote)
     res.render('../views/tuotteet', {products: tuote})
   })
   .catch(err => {
-    console.log(err.message)
-    res.status(500).send({ message: err.message || "Error" })
+    console.log(err)
+    res.render('../views/error')
   })
 };
