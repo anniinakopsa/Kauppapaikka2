9921d655a457e8bc46c2dc5802463779b997d5f0
Anniina Kopsa <anniina@softarobotit.fi>
2024-11-05 13:19:08 +0200

    admin polut kuntoon

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,8 @@ const tuotteet = require('./tuotteet')
 const tuote = require('./tuote')
 const adminTuotteet = require('./adminTuotteet') // admin tuotteiden listaus
 const uusiTuote = require('./uusiTuote') // admin tuotteen lisäys lomake
-const apiLisaaTuote = require('./apiLisaaTuote')
+const adminLisaaTuote = require('./adminLisaaTuote') // admin tuotteen lisäys tietokantaan
+const apiLisaaTuote = require('./apiLisaaTuote') // api rajapinta tuotteen lisäys tietokantaan
 const eiloydy = require('./404')
 
 router.get('/', etusivu)
@@ -16,6 +17,7 @@ router.get('/tuotteet', tuotteet)
 router.get('/tuote/:id', tuote)
 router.get('/admin', adminTuotteet) // admin tuotteiden listaus
 router.get('/admin/tuote', uusiTuote) // admin tuotteen lisäys lomake
+router.get('/admin/tuote/lisaa', adminLisaaTuote) // admin tuotteen lisäys tietokantaan
 router.post('/api/tuote', apiLisaaTuote) // api rajapinta tuotteen lisäys tietokantaan
 router.get('*', eiloydy)
 
