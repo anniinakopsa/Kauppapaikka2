587225fe4771397fe2e1b4ecfee5e2d4e2a863a4
Anniina Kopsa <anniina@softarobotit.fi>
2024-11-05 10:18:35 +0200

    tuote näkymä

--- routes/tuote.js
+++ routes/tuote.js
@@ -1,3 +1,14 @@
+let Tuote = require('../model/tuote')
+
 module.exports = (req, res) => {
-  res.send('Tuote ' + req.params.id + " valittu")
+
+  Tuote.find({ tuoteid: req.params.id })
+  .then(tuoteHaku => {
+    console.log(tuoteHaku)
+    res.render('../views/tuote', {products: tuoteHaku})
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('../views/error')
+  })
 };
